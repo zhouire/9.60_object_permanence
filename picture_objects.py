0ff72281024faf91ffffcb7cc4d97e59fbb849c1
monaabd27 <monaabd@mit.edu>
2020-04-11 17:03:36 -0500

    object and background generation

--- picture_objects.py
+++ picture_objects.py
@@ -11,17 +11,44 @@
 #yolo takes square images from 320 to 608, with a step of 32
 from PIL import Image, ImageDraw, ImageFilter
 image_size = 320
-def create_shape(percentage, shape):
-    shape_size = int(image_size*percentage)
+def create_shape(percentage, shape, color = 'white'):
+    if shape == 'circle':
+        shape_size = int(image_size*percentage)
+        shape = Image.new('RGBA',(shape_size, shape_size), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(shape)
+        draw.ellipse([(0, 0),(shape_size, shape_size)], fill=color)
+        return shape
+    elif shape == 'rectangle':
+        shape_size = int(image_size*percentage)
 
+        shape = Image.new('RGBA',(int(shape_size/3), shape_size), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(shape)
+        draw.rectangle([(0, 0),(int(shape_size/3), shape_size)], fill=color)
+        return shape
+    elif shape == 'triangle':
+        shape_size = int(image_size*percentage)
+        shape = Image.new('RGBA',(shape_size, shape_size), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(shape)
+        draw.polygon([(int(shape_size/2), 0),(0, shape_size),(shape_size, shape_size)], fill=color)
+        return shape
+    else:
+        raise Exception('not a valid shape type-choose between circle, rectangle, triangle')
 
 #one function to generate the backgrounds
 def create_background(color = 'gray'):
-    pass
-
+    background =  Image.new('RGBA',(image_size, image_size), color)
+    return background
+    
 #one function to generate the occlusions
 def create_occlusion(orientation, shape_size, color = 'black'):
-    pass
+    if orientation=='horizontal':
+        pass
+    
+    elif  orientation=='horizontal':
+        pass
+    else:
+        raise Exception('not a valid shape type-choose between horizontal or verticle')
+
 
 #one function to combine images together (for stationary objects)
 def combine_sb(shape, background, rotate_degree = 0):
@@ -30,3 +57,7 @@ def combine_sb(shape, background, rotate_degree = 0):
 #another function to add in labels and bounding boxes
 def labels(image):
     pass
+
+test = create_shape(0.33, 'triangle')
+test.save('test.png')
+test.show()
