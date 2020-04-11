60dc66b1cee7756397176b1897fc05ba4541b52d
monaabd27 <monaabd@mit.edu>
2020-04-11 18:47:25 -0500

    added in bounding box and label generation

--- picture_objects.py
+++ picture_objects.py
@@ -12,25 +12,25 @@ from PIL import Image, ImageDraw, ImageFilter
 image_size = 320
 
 #one function to generate the objects
-def create_shape(percentage, shape, color = 'white'):
-    if shape == 'circle':
+def create_shape(percentage, shape_type, color = 'white'):
+    if shape_type == 'circle':
         shape_size = int(image_size*percentage)
         shape = Image.new('RGBA',(shape_size, shape_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(shape)
         draw.ellipse([(0, 0),(shape_size, shape_size)], fill=color)
-        return shape
-    elif shape == 'rectangle':
+        return shape, shape_type
+    elif shape_type == 'rectangle':
         shape_size = int(image_size*percentage)
         shape = Image.new('RGBA',(int(shape_size/3), shape_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(shape)
         draw.rectangle([(0, 0),(int(shape_size/3), shape_size)], fill=color)
-        return shape
-    elif shape == 'triangle':
+        return shape, shape_type
+    elif shape_type == 'triangle':
         shape_size = int(image_size*percentage)
         shape = Image.new('RGBA',(shape_size, shape_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(shape)
         draw.polygon([(int(shape_size/2), 0),(0, shape_size),(shape_size, shape_size)], fill=color)
-        return shape
+        return shape, shape_type
     else:
         raise Exception('not a valid shape type-choose between circle, rectangle, triangle')
 
@@ -50,7 +50,7 @@ def create_occlusion(orientation, shape_size, color = 'black'):
     elif  orientation=='vertical':
         occ = Image.new('RGBA',(int(shape_size/3), shape_size), (0, 0, 0, 0))
         draw = ImageDraw.Draw(occ)
-        draw.rectangle([(int(shape_size/3), shape_size), (0, 0),], fill=color)
+        draw.rectangle([(int(shape_size/3), shape_size), (0, 0)], fill=color)
         return occ
     else:
         raise Exception('not a valid shape type-choose between horizontal or vertical')
@@ -58,13 +58,23 @@ def create_occlusion(orientation, shape_size, color = 'black'):
 
 #one function to combine images together (for stationary objects)
 def combine_sb(shape, background, rotate_degree = 0):
-    pass
+    bs = background.size
+    ss = shape[0].size
+    loc = (int(bs[0]/2)-int(ss[0]/2),int(bs[1]/2)-int(ss[1]/2))
+    background.paste(shape[0], loc, shape[0])
+    height = ss[1]
+    width = ss[0]
+    top_left = loc
+    bottom_right = (loc[0]+width, loc[1]+height)
+    bounding_box = (top_left, bottom_right, width, height)
+    annotation = (bounding_box, shape[1])
+    return background, annotation
 
-#another function to add in labels and bounding boxes
-def labels(image):
-    pass
 
-shape = create_shape(0.33, 'triangle', color='purple')
-shape_size = shape.size
-test = create_occlusion('vertical', max(shape_size), color = 'black')
-test.show()
+#testing to see if stuff works
+shape = create_shape(0.33, 'rectangle', color='purple')
+shape_size = shape[0].size
+background = create_background()
+test = combine_sb(shape, background)
+test[0].show()
+print(test[1])
