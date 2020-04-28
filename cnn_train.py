0758654f51f2cc2081dad624f6cb09548c47960b
Irene Zhou <zhoui@MIT.EDU>
2020-04-28 17:04:07 -0400

    bug fixes for GPU training

--- cnn_train.py
+++ cnn_train.py
@@ -16,8 +16,8 @@ train_set = ShapeImageDataset(train_images, train_labels, yolo=False, transform=
 #Test loader has constant batch sizes, so we can define it directly
 #test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=True, num_workers=2)
 
-epochs = 1
-batch_size = 128
+epochs = 10
+batch_size = 64
 lr = 0.001
 
 def get_train_loader(batch_size):
@@ -43,10 +43,10 @@ if __name__ == "__main__":
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data['image'].to(device), data['annotation'].to(device)
+            inputs, labels = data['image'], data['annotation']
             # change ByteTensor (default) to FloatTensor
-            inputs = inputs.type('torch.FloatTensor')
-            labels = labels.squeeze(1).type('torch.LongTensor')
+            inputs = inputs.type('torch.FloatTensor').to(device)
+            labels = labels.squeeze(1).type('torch.LongTensor').to(device)
             print(labels)
 
             # zero the parameter gradients
@@ -61,12 +61,12 @@ if __name__ == "__main__":
 
             # print statistics
             running_loss += loss.item()
-            if i % 1 == 0:  # print every 2000 mini-batches
+            if i % 20 == 0:  # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 1))
+                      (epoch + 1, i + 1, running_loss / 20))
                 running_loss = 0.0
 
-    PATH = 'trained_models/cnn_net.pt'
+    PATH = 'trained_models/cnn_net_10epoch.pt'
     torch.save(net.state_dict(), PATH)
 
     print('Finished Training')
