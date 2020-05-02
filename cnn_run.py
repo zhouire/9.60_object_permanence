e2c6c9e41ce2c5ba1a88ad6b9ee93677c48585ad
Irene Zhou <zhoui@MIT.EDU>
2020-05-02 19:30:39 -0400

    fix data loading in cnn run

--- cnn_run.py
+++ cnn_run.py
@@ -5,9 +5,11 @@ from torchvision import transforms, utils
 from cnn_feature_extraction import CNN
 from video_dataset import VideoCNNDataset, ToTensor
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 net = CNN()
-net.load_state_dict(torch.load("trained_models/cnn_net_10epoch.pt", map_location=torch.device("cpu")))
+net.load_state_dict(torch.load("trained_models/cnn_net_10epoch.pt", map_location=device))
 
 test_images = "data/videos/allimages.txt"
 test_set = VideoCNNDataset(test_images, transform=transforms.Compose([ToTensor()]))
@@ -18,7 +20,7 @@ total = 0
 correct = 0
 with torch.no_grad():
     for data in test_loader:
-        images, paths, labels = data
+        images, paths, labels = data["image"], data['path'], data['annotation']
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
